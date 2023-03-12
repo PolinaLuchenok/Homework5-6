7be1122a4dd1f83f41968e43c68c1d6044db8982
MyComputer <MyComputer@192.168.100.204>
2023-03-13 01:45:31 +0300

    added hw5

---
+++ hw5/src/test/java/hw5/TestFilterHairdryers.java
@@ -0,0 +1,67 @@
+package hw5;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TestFilterHairdryers {
+	
+	private static ChromeOptions chromeOptions = new ChromeOptions();
+	private static WebDriver driver;
+	
+	@Test
+	public void testFilterBrand() {
+		chromeOptions.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\drivers\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver(chromeOptions);
+        
+        driver.get("https://onliner.by");
+        driver.findElement(By.xpath("//span[text()='Фены']")).click();
+        ((JavascriptExecutor)driver).executeScript("window.scrollBy(0, 250)");
+        driver.findElement(By.xpath("//input[@value='rowenta']/following-sibling::span")).click();
+        
+        Wait<WebDriver> wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.urlToBe("https://catalog.onliner.by/hairdryer?mfr%5B0%5D=rowenta"));
+        
+        List<WebElement> listElements = driver.findElements(By.xpath("//div[@class='schema-product__title']//span[contains(text(), 'Rowenta')]"));
+        for (WebElement ellemm : listElements) {
+        	Assertions.assertTrue(ellemm.getText().contains("Rowenta"));
+        }
+        
+        driver.quit();
+	}
+	
+	@Test
+	public void testFilterBrands() {
+		chromeOptions.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\drivers\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        
+        driver.get("https://onliner.by");
+        driver.findElement(By.xpath("//span[text()='Фены']")).click();
+        ((JavascriptExecutor)driver).executeScript("window.scrollBy(0, 350)");
+        driver.findElement(By.xpath("//input[@value='dyson']/following-sibling::span")).click();
+        driver.findElement(By.xpath("//div[@class='schema-filter-control__item']")).click();
+        driver.findElement(By.xpath("//div[@class='schema-filter-popover__column-item']//span[text()='Braun']")).click();
+        
+        Wait<WebDriver> wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.urlToBe("https://catalog.onliner.by/hairdryer?mfr%5B0%5D=dyson&mfr%5B1%5D=braun"));
+        
+        List<WebElement> listElements = driver.findElements(By.xpath("//div[@class='schema-product__title']//span"));
+        for (WebElement ellemm : listElements) {
+        	Assertions.assertTrue(ellemm.getText().contains("Dyson") || ellemm.getText().contains("Braun"));
+        }
+        
+        driver.quit();
+	}
+}
